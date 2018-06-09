51b42eeb28f04e0f0f147d7922ec40e489df224c
League Student <league@iMac-8.attlocal.net>
2018-06-08 17:01:41 -0700

    yash is tryhard
    tryhard

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,44 @@
 package IntroToArrayLists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook extends JFrame implements ActionListener {
+	ArrayList<String> book = new ArrayList<>();
+	JButton button2;
+	JButton button;
+	public GuestBook() {
+		JPanel panel = new JPanel();
+		add(panel);
+		button = new JButton("add name");
+		button2 = new JButton("view names");
+		button.addActionListener(this);
+		button2.addActionListener(this);
+		panel.add(button);
+		panel.add(button2);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+	}
+	public void iterate() {
+		String guests = "";
+		for(int i = 0; i < book.size(); i++){
+			String s = book.get(i);
+			guests += "Guest #" + (i + 1) + ": " + s + "\n";
+		}
+		JOptionPane.showMessageDialog(null, guests);
+	}
+	public static void main(String[] args) {
+		GuestBook guests = new GuestBook();
+		
+		
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +47,15 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==button) {
+			book.add(JOptionPane.showInputDialog("Type in a name"));
+		}
+		else {
+			iterate();
+		}
+	}
 }
--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,14 +1,26 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> names = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		names.add("laffyette");
+		names.add("mulligan");
+		names.add("hamilton");
+		names.add("burr");
+		names.add("lawrence");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = names.size()-1; i >= 0; i--) {
+			String s = names.get(i);
+			if(i%2==0) {
+				System.out.println(i + ". " + s);
+			}
+		}
+		System.out.println("\n");
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
