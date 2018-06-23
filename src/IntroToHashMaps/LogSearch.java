fb766b5c641c59ed3a7c6b1f81e86c2dc3f6569c
is8bella <league@iMac-6.attlocal.net>
2018-06-22 17:00:25 -0700

    finished intro to hash maps
    yash ditched

--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -1,6 +1,44 @@
 package IntroToHashMaps;
 
-public class LogSearch {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class LogSearch implements ActionListener {
+	static HashMap<Integer, String> log = new HashMap<Integer, String>();
+	JFrame frame;
+	JPanel panel;
+	JButton button;
+	JButton button2;
+	JButton button3;
+	JButton button4;
+	public LogSearch() {
+		frame = new JFrame();
+		panel = new JPanel();
+		button = new JButton("Add Entry");
+		button2 = new JButton("Search by ID");
+		button3 = new JButton("View List");
+		button4 = new JButton("Remove Entry");
+		frame.add(panel);
+		panel.add(button);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		button.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		button4.addActionListener(this);
+		frame.setVisible(true);
+		frame.pack();
+	}
+	public static void main(String[] args) {
+		LogSearch search = new LogSearch();
+	}
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -28,4 +66,38 @@ public class LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	public static void iterate() {
+		for(int i: log.keySet()) {
+			System.out.println("ID: " + i + " Name: " + log.get(i));
+		}
+	}
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(button)) {
+			String id = JOptionPane.showInputDialog("Enter an ID number");
+			String name = JOptionPane.showInputDialog("Enter a name");
+			log.put(Integer.parseInt(id), name);
+		}
+		else if(e.getSource().equals(button2)) {
+			String id = JOptionPane.showInputDialog("Search an ID number");
+			if(log.keySet().contains(Integer.parseInt(id))) {
+				JOptionPane.showMessageDialog(null, log.get(Integer.parseInt(id)));
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "ID number is not in the database");
+			}
+		}
+		else if(e.getSource().equals(button3)) {
+			iterate();
+		}
+		else if(e.getSource().equals(button4)) {
+			String id = JOptionPane.showInputDialog("Enter an ID number to remove");
+			if(log.keySet().contains(Integer.parseInt(id))) {
+				log.remove(Integer.parseInt(id));
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "ID number is not in the database");
+			}
+		}
+	}
 }
